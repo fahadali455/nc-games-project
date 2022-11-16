52b6e1c0920ce510a92af07aa76bd51ccb147048
Fahad Ali <fahad_01@hotmail.co.uk>
2022-11-16 11:35:19 +0000

    6. GET /api/reviews/:review_id/comments solution

--- routes/reviews-router.js
+++ routes/reviews-router.js
@@ -1,5 +1,5 @@
 const reviewsRouter = require("express").Router();
-const { getReviews, getReviewById } = require("../models/reviews");
+const { getReviews, getReviewById, getCommentsByReviewId } = require("../models/reviews");
 
 reviewsRouter
     .route("/")
@@ -18,6 +18,16 @@ reviewsRouter
 
     })
     
+reviewsRouter
+    .route("/:review_id/comments")
+    .get((req, res, next) => {
+        const { review_id } = req.params;
 
+        getCommentsByReviewId(review_id)
+        .then(comments => {
+            res.send({ comments })
+        })
+        .catch(next)
+    })
 
 module.exports = reviewsRouter;
