4ebf515b5bb19e9074ecb04ced974149811a1edc
Fahad Ali <fahad_01@hotmail.co.uk>
2022-11-16 09:07:50 +0000

    5. get /api/reviews/:review_id solution

--- models/reviews.js
+++ models/reviews.js
@@ -13,3 +13,23 @@ exports.getReviews = () => {
     )
     .then(reviews => {return reviews.rows})
 }
+
+exports.getReviewById = (reviewId) => {
+    if(Number.isInteger(Number(reviewId))){
+        return db
+            .query(
+                `SELECT * FROM reviews
+                WHERE review_id = $1;
+                `, [reviewId]
+            )
+            .then(review => {
+                if(review.rows.length === 0){
+                    return Promise.reject({status: 404, msg: "Resource not found."})
+                }
+                return review.rows[0]
+            })
+    }
+    else{
+        return Promise.reject({ status: 400, msg: "Bad Request!" });
+    }
+}
