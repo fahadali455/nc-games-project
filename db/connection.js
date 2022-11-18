40179eac6a1b1c7d319213a9fadce62b0ff23f22
Fahad Ali <fahad_01@hotmail.co.uk>
2022-11-18 16:28:05 +0000

    hosting changes

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,12 @@
 const { Pool } = require('pg');
+
+
 const ENV = process.env.NODE_ENV || 'development';
+
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
@@ -7,12 +14,9 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 
 const config = ENV === "production" ? { connectionString: process.env.DATABASE_URL,} : {};
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
-}
+// if (!process.env.PGDATABASE) {
+//   throw new Error('PGDATABASE not set');
+// }
 
 module.exports = new Pool(config);
